b4bf1b1016e94ae4e859c38c80a8f0030e8925ba
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2023-01-23 16:27:18 +0200

    Update ContactList.jsx

--- src/components/PhoneBook/ContactList/ContactList.jsx
+++ src/components/PhoneBook/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+
 import {DeleteBtn} from './ContactList.styled';
 import { useDispatch } from 'react-redux';
 import { removeContact } from 'redux/operations';
@@ -23,13 +23,3 @@ export default function PhoneBookList({ items }) {
   return <ul>{elements}</ul>;
 }
 
-// PhoneBookList.propTypes = {
-//   items: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//       id: PropTypes.string.isRequired,
-//     })
-//   ),
-//   removeContact: PropTypes.func.isRequired,
-// };
